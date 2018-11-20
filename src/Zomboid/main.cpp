7df13e93cefd8192d7788b661f3a97fcb336f0de
Coestaris <vk_vm@ukr.net>
2018-11-20 20:37:14 +0200

    Refactoring

--- src/Zomboid/main.cpp
+++ src/Zomboid/main.cpp
@@ -4,6 +4,7 @@ int main(int argc, char **argv)
 {
     Updater::init();
     EventHandler::init(argc, argv, Vector2i(500, 500), Vector2i(100, 100), "Game title");
-
     EventHandler::runGame();
 }
+
+
